bf353a58060e93946f50190ced555af0b974221e
Sergio Guerra <sergioguerra6188@gmail.com>
2022-12-16 12:03:01 -0500

    Create conditional that assigns string to "choice"
    Used if, else-if, and else as there are three strings that can be
    assigned to choice. Each string is assigned based on the randomNum that
    is generated. This way, the computer's choice is always random.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,10 +1,19 @@
 function getComputerChoice() {
     //Choose a number between and including 1-3
     let randomNum = Math.floor(Math.random() * 3) + 1;
-    
+    let choice;
 
-    console.log(randomNum);
-    return randomNum;
+    if (randomNum === 1) {
+        choice = "Rock";
+    }
+    else if (randomNum === 2) {
+        choice = "Paper";
+    }
+    else {
+        choice = "Scissors";
+    }
+    
+    return choice;
 }
 
 getComputerChoice();
