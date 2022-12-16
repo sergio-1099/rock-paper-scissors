84fbe275b9fac7f29ff5267d09f18a265990effc
Sergio Guerra <sergioguerra6188@gmail.com>
2022-12-16 11:59:27 -0500

    Change variable name to "randomNum" in function
    The variable name "choice" will be reserved for the actual choice of
    rock paper or scissors. I changed the variable name for the number to
    randomNum as this is more appropriate.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,10 @@
 function getComputerChoice() {
     //Choose a number between and including 1-3
-    let choice = Math.floor(Math.random() * 3) + 1;
-    console.log(choice);
-    return choice;
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    
+
+    console.log(randomNum);
+    return randomNum;
 }
 
 getComputerChoice();
